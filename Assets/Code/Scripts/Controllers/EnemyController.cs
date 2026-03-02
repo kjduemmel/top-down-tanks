8d78a18b11f8d3b7a8a7be7bfd455c44743ac5cc
Aaron Wiens <bubba.wiens@gmail.com>
2026-03-02 13:59:21 -0600

    Updated the Player Tank scene with new Item, made UI a scene, UI tracks enemies alive

--- Assets/Code/Scripts/Controllers/EnemyController.cs
+++ Assets/Code/Scripts/Controllers/EnemyController.cs
@@ -39,6 +39,7 @@ public partial class EnemyController : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		AddToGroup("enemyTanksGroup");
 		reloadTimer = ReloadTime;
 		
 		ui = GetTree().GetCurrentScene().GetNode<UI>("UI");
--- Assets/Code/Scripts/UI.cs
+++ Assets/Code/Scripts/UI.cs
@@ -10,19 +10,27 @@ public partial class UI : CanvasLayer
 	Label ScoreLabel;
 	
 	[Export]
+	Label EnemiesLabel;
+	
+	[Export]
 	Timer MessageTimer;
 	
 	int score = 0;
+	int numEnemies = 0;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		ScoreLabel.Text = $"Score: {score}";
+		numEnemies = GetTree().GetNodeCountInGroup("enemyTanksGroup");
+		EnemiesLabel.Text = $"Enemies: {numEnemies}";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		numEnemies = GetTree().GetNodeCountInGroup("enemyTanksGroup");
+		EnemiesLabel.Text = $"Enemies: {numEnemies}";
 	}
 	
 	public void ShowMessage(string message)
